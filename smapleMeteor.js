e027c1e6b21426cc27236dbd47d3e4dd5f442308
Addison Freeman <addison.freeman@trinity.edu>
2015-12-02 18:59:14 -0600

    making changes to how values are stored and read, not submitting currently. Check: preventDefault, re-setting an array in session storage.

--- smapleMeteor.js
+++ smapleMeteor.js
@@ -1,6 +1,6 @@
-
-//Setup db
+//Setup db Collections
 var Schemas = {};
+
 items = new Mongo.Collection('items');
 Schemas.itemsSchema = new SimpleSchema({
 	itemId: {
@@ -14,80 +14,115 @@ Schemas.itemsSchema = new SimpleSchema({
 items.attachSchema(Schemas.itemsSchema);
 	
 fields = new Mongo.Collection("fields");
-
-Schemas.fields = new SimpleSchema({
+Schemas.fieldsSchema = new SimpleSchema({
 	item_id: {
 		type: String
 	},
-
-	fieldA: {
-		type: String,
-		optional: true
-	},
-	fieldB: {
-		type: String,
-		optional: true
-	},
-	fieldC: {
-		type: String,
+	fields: {
+		type: [String],
 		optional: true
 	}
 });
-fields.attachSchema(Schemas.fields);
+fields.attachSchema(Schemas.fieldsSchema);
 	
+fieldNames = new Mongo.Collection("fieldNames");
+Schemas.fieldNameSchema = new SimpleSchema({
+	fieldName: {
+		type: String
+	}
+});
+fieldNames.attachSchema(Schemas.fieldNameSchema);
+	
+
 if (Meteor.isServer) {
 	Meteor.startup(function() {
 		return Meteor.methods({
-		emptyCollections: function() {
-			items.remove({});
-			return fields.remove({});
-		}
+			//empty collections on startup
+			emptyCollections: function() {
+				items.remove({});
+				fieldNames.remove({});
+				return fields.remove({});
+			},
+			instaDomIds: function() {
+				var domIds = [];
+				return Session.set("DomIds", domIds);
+			},
+			instaFieldNames: function() {
+				var fieldNameArr = [];
+				return Session.set("fieldNameArray", fieldNameArray)
+			}
 		});
 	});
 }
+
 if(Meteor.isClient) {
 	Template.TemplateA.helpers({
+		//returns a cursor of all items to store field values for
 		findItems: function() {
-			return items.find({},{})
+			return items.find({},{});
+		},
+		//returns a cursor of field names to append to each item
+		//TODO: add item name to schema to allow different values for different items
+		//      ^-- retrieve from collection elsewhere?
+		fieldNames: function() {
+			return fieldNames.find({},{});
 		}
 	});
 	
-	
-	Template.TemplateA.events({
-	"keydown .setVal": function (event) {
-		var value = $(event.target).val();
-		Session.set(event.target.name, value);
-	}
+	//return a field value if it was previously stored by id+field key
+	UI.registerHelper('getValue', function(id, field) {
+		  return Session.get(id+field);
+	});
+
+	//passes item ids into a session variable array, must update records with new instance of array (safe because Session.get returns clone of value)
+	UI.registerHelper('storeDomId', function(item_id, options) {
+		//(x && ...) checks existence before returning what's after
+		  return Session.get("DomIds") && Session.set("DomIds", Session.get("DomIds").push(item_id));
+		//} 	  
 	});
 	
-	Template.TemplateA.events({
-		"submit form": function (event, template) {
-			event.preventDefault(); //don't ajax or post anything that submit might do
-			console.log(event);
-			var domIds = document.getElementsByClassName("item_id");
-			console.log(domIds);
-			for (i = 0; i < domIds.length; i++) {
-				var item_id = domIds[i].value;
-				var fieldA = Session.get(item_id+"fieldA");
-				var fieldB = Session.get(item_id+"fieldB");
-				var fieldC = Session.get(item_id+"fieldC");
-			
-				fields.insert({
-					item_id: item_id,
-					fieldA: fieldA,
-					fieldB: fieldB,
-					fieldC: fieldC,
-				});
-			//Meteor.call (addbid, postjob_id, currentUser, bid, turntim, item_ide, turn false;
-			}
+	UI.registerHelper('storeFieldName', function(fieldName, options) {
+		//(x && ...) checks existence before returning what's after
+		  return Session.get("fieldNameArray") && Session.set("fieldNameArray", Session.get("fieldNameArray").push(fieldName));
+		//} 	  
+	});
+	
+	//stores field values by unique field name on key press
+	Template.TemplateB.events({
+		"keydown .setVal": function (event) {
+			var value = $(event.target).val();
+			Session.set(event.target.name, value);
 		}
 	});
 	
+	//empties all database collections (not Session storage), must be called from server
 	Template.TemplateA.events({
 		"reset form": function (event, template) {
-			event.preventDefault();
+			event.defaultPrevented;
 			Meteor.call("emptyCollections");
 		}
 	});
-	
+
+	//iterate through fields by id from idArray (both stored in Session storage) and copy into database
+	Template.TemplateA.events({
+		"submit form": function (event, template) {
+			event.defaultPrevented; //don't ajax or post anything tha
+			//can't it read from db source itself? b/c not "reactive"
+			var domIds = Session.get("domIds");
+			for (i = 0; i < domIds.length; i++) {
+				var item_id = domIds[i].value;
+				//get (with id) for each fieldName
+				var fieldNameArray = Session.get("fieldNameArray");
+				var fieldsTemp = [];
+				for (i = 0; j < fieldNameArray.length; j++) {
+					fieldsTemp.add(Session.get(item_id+fieldNameArray[j]));
+						
+					fields.insert({
+						item_id: item_id,
+						fieldsTemp: fields
+					});
+				}
+			}
+		}
+	});	
 }
